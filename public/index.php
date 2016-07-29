4e36964b723f87ccee95e42a534b58869b596940
ariane-lozachmeur <ariane.lozachmeur@gmail.com>
2016-07-29 14:34:22 +0800

    Test

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 |--------------------------------------------------------------------------
 | Turn On The Lights
 |--------------------------------------------------------------------------
-|
+|This is a change
 | We need to illuminate PHP development, so let us turn on the lights.
 | This bootstraps the framework and gets it ready for use, then it
 | will load up this application so that we can run it and send
